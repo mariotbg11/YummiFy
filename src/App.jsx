1e68dde5bc13b56178b24cb1ba1fca5f9aad40e2
mariotbg11 <christofell.m@gmail.com>
2024-10-20 21:36:33 +0700

    create card list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import NavbarWithSearch from "./components/Navbar";
 import Hero from "./components/Hero";
-import CardItem from "./components/CardItem";
+import CardList from "./components/CardList";
 
 function App() {
   return (
     <div className="max-w-screen-2xl mx-auto px-3">
       <NavbarWithSearch />
       <Hero />
-      <CardItem />
+      <CardList />
     </div>
   );
 }
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -11,7 +11,7 @@ import {
 
 function CardItem() {
   return (
-    <Card className="w-full max-w-[26rem] shadow-lg">
+    <Card className="w-full shadow-lg">
       <CardHeader floated={false} color="blue-gray">
         <img
           src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
