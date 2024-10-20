d605482b1f537f459ea884ea6bf8c739bfb6a66a
mariotbg11 <christofell.m@gmail.com>
2024-10-20 21:48:57 +0700

    create card item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import NavbarWithSearch from "./components/Navbar";
 import Hero from "./components/Hero";
-import CardItem from "./components/CardItem";
 
 function App() {
   return (
     <div className="max-w-screen-2xl mx-auto px-3">
       <NavbarWithSearch />
       <Hero />
-      <CardItem />
     </div>
   );
 }
