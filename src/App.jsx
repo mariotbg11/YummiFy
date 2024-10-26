038294cd47c8cc385126266bb22aab0e989117ac
mariotbg11 <christofell.m@gmail.com>
2024-10-26 16:16:00 +0700

    feat: create stats component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import CardList from "./components/CardList";
 import AccordionFAQ from "./components/AccordionFAQ";
 import Footer from "./components/Footer";
 import Newsletter from "./components/Newsletter";
+import Stats from "./components/Stats";
 
 function App() {
   return (
     <div className="max-w-screen-2xl mx-auto px-3">
       <NavbarWithSearch />
       <Hero />
+      <Stats />
       <CardList />
       <AccordionFAQ />
       <Newsletter />
